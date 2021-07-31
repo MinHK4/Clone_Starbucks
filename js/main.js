94dc16862ef435f66104a34870151697d0571ea7
MinHK4 <dzf4444@gmail.com>
2021-07-31 10:18:29 +0900

    common moudle split

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
-// header-search
-const searchEl = document.querySelector('.search');
-const searchInputEl = searchEl.querySelector('input');
-searchEl.addEventListener('click', function(){
-  searchInputEl.focus();
-});
-searchInputEl.addEventListener('focus', function(){
-  searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder', '통합검색')
-})
-searchInputEl.addEventListener('blur', function(){
-  searchEl.classList.remove('focused');
-  searchInputEl.removeAttribute('placeholder', '통합검색')
-})
-
-
 // drop down & Window Scrollto btn
 // _.throttle(function, time)
 // gsap(element, time, option)
@@ -155,7 +139,3 @@ spyEls.forEach( function (spyEl){
     .setClassToggle(spyEl, 'show')
     .addTo(new ScrollMagic.Controller());
 });
-
-// FOOTER this-year
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
